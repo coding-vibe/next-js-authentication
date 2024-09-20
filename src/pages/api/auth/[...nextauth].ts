10096294f52607807bd1a8d656849efc0b1826f4
Ihor Sokolov <i.sokolov@geniusee.com>
2024-09-20 14:37:47 +0300

    add credentials provider

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+interface LoginResponse {
+  access: string;
+  refresh: string;
+}
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+
+      async authorize(credentials) {
+        try {
+          if (!credentials) {
+            return null;
+          }
+
+          const { email } = credentials;
+
+          const {
+            data: { access },
+          } = await axios.post<LoginResponse>(
+            `${process.env.NEXT_PUBLIC_API_HOST}/users/login/`,
+            credentials,
+          );
+          const { data: user } = await axios.get(
+            `${process.env.NEXT_PUBLIC_API_HOST}/users/${email}/`,
+            {
+              headers: { Authorization: `Bearer ${access}` },
+            },
+          );
+
+          return user;
+        } catch {
+          return null;
+        }
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
