b23cb061658aedaa2eed1c04a8beef48260d8cb2
Ihor Sokolov <i.sokolov@geniusee.com>
2024-10-03 00:42:33 +0300

    use updated session

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+import Button from '@mui/material/Button';
 
 const ProtectedPage: FC = () => {
   const { data } = useSession();
 
-  return <div>Hello {data?.user?.email}!</div>;
+  return (
+    <>
+      <div>Hello {data?.user?.first_name}! </div>
+      <Button onClick={() => signOut()}>Sign out</Button>
+    </>
+  );
 };
 
 export default ProtectedPage;
